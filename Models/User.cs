b6ecc007fdb8bd2659ee155bc34b518f88f850d1
Ahmad Saad <Ahmad Saad@DESKTOP-R6OORT1>
2024-04-03 00:29:30 +0500

    start

---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+
+namespace eCommerce.Models
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public Address ShippingAddress { get; set; }
+        public string ProfilePicture { get; set; }
+        public DateTime RegistrationDate { get; set; }
+
+    }
+}
