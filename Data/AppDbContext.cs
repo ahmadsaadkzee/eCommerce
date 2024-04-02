a7eb9b370ef2790ef2247ac39d03bfa0832c7f55
Ahmad Saad <ahmadsaadkz@gmail.com>
2024-04-03 01:45:39 +0500

    Create AppDbContext.cs

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace eCommerce.Data
+{
+    public class AppDbContext: DbContext
+
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) :
+        {
+
+
+        }
+
+    }
+}
